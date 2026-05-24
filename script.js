cc047e4f4f2f086b72238b788b2e60e18baacae0
Tim <nadioluwatimilehin@gmail.com>
2026-05-24 18:46:31 +0100

    task add and delete feature

--- script.js
+++ script.js
@@ -6,28 +6,24 @@ let emptyState = document.getElementById('emptyState');
 
 let taskList = document.getElementById('taskList');
 
+// Default List
+let copyTask = [
+    {id:1, task:'Put the add button', to:'Tim'},
+    {id:2, task:'Style the list', to:'Sam'},
+    {id:3, task:'return', to:'Tee'},
+]
+
+
 
 function addTask() {
     
     var taskVal = taskInput.value.trim();
-    if (taskVal && userInput.value.trim()){
-        let taskItem = document.createElement('li');
-        taskItem.textContent = `${userInput.value.trim()} --> ${taskVal}` ;
+    var userVal = userInput.value.trim();
 
-        let deltask = document.createElement('button');
-        deltask.textContent = 'x'
+    if (taskVal && userVal){
+        copyTask = [...copyTask,{id:Date.now(),task:taskVal,to:userVal}]
 
-        taskList.appendChild(taskItem);
-        
-        taskItem.setAttribute('class','taskItem');
-        
-        taskItem.appendChild(deltask);
-
-       
-        deltask.addEventListener('click', function del(){
-            taskList.removeChild(taskItem);
-            itemCheck();
-        })
+        updateList();
 
         console.log('Hello world');
     } else{alert('input an assigned user and a task son')}
@@ -41,14 +37,51 @@ function addTask() {
 
 addTaskBtn.addEventListener('click', function () { addTask();});
 
+function updateList() {
+    //Empty the dom list
 
-// BUG to check amount of items in tasak list
+    taskList.replaceChildren()
+
+    for (let i = 0; i < copyTask.length; i++) {
+        
+        const element = copyTask[i];
+        
+        let taskItem = document.createElement('li');
+        taskItem.textContent = `${element.task} --> ${element.to}` ;
+
+        let deltask = document.createElement('button');
+        deltask.textContent = 'x'
+
+        taskList.appendChild(taskItem);
+        
+        taskItem.setAttribute('class','taskItem');
+        
+        taskItem.appendChild(deltask);
+
+       
+        deltask.addEventListener('click', function (){ del(taskItem,element); })
+    }
+}
+
+updateList()
+
+function del(taskItem,element){
+    
+    let ind = copyTask.findIndex(p => p.id == element.id);
+
+    copyTask.splice(ind,1);
+    
+    itemCheck();
+
+    updateList();
+}
+
 function itemCheck(){
     if (taskList.children.length == 0) {
     emptyState.style.display = 'block';
-}else {
-    emptyState.style.display = 'none';
-}
+    } else {
+        emptyState.style.display = 'none';
+    }
 }
 
 console.log(taskList.children.length)
